399cab5137797fe7703b6bbee42be72a3b532dcd
Brian Lynch <bglynch17@gmail.com>
2018-08-19 09:49:28 +0000

    reduced explore blogs posts to 3

--- microblog/config.py
+++ microblog/config.py
@@ -10,4 +10,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['bglynchtest@gmail.com']    
+    ADMINS = ['bglynchtest@gmail.com']
+    POSTS_PER_PAGE = 3
